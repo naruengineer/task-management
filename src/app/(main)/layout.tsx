bf036dd3ddae91b9b8ec682b5c4ff897dd13d63a
NarukiAndo <5050naruboworld00@gmail.com>
2024-07-06 00:07:27 +0900

    viewportの修正

--- src/app/(main)/layout.tsx
+++ src/app/(main)/layout.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import SideMenu from "../../components/SideMenu/SideMenu";
+import { ControlViewport } from "@/components/ViewPoint/controlViewPoint";
 
 const Mainlayout = ({
   children,
@@ -8,6 +9,7 @@ const Mainlayout = ({
 }>) => {
   return (
     <div className="flex h-screen">
+      <ControlViewport />
       <SideMenu />
       <main className="bg-gray-300 flex-1 overflow-auto">{children}</main>
     </div>
