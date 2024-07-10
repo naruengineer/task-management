db04e89c7b788ec0caa8487ff3125c6f68a84999
NarukiAndo <5050naruboworld00@gmail.com>
2024-07-10 14:00:47 +0900

    done

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -14,7 +14,6 @@ const handler = NextAuth({
           prompt: "consent",
           access_type: "offline",
           response_type: "code",
-          redirect_uri: `${process.env.NEXTAUTH_URL}/api/auth/callback/google`,
         },
       },
     }),
