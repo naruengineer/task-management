75798fca3c8b086dfaf0d1ad5e8e0a7a74c6d64f
NarukiAndo <5050naruboworld00@gmail.com>
2024-07-11 14:40:07 +0900

    done

--- src/utils/database.ts
+++ src/utils/database.ts
@@ -3,6 +3,7 @@ import mongoose from "mongoose";
 export const connectDB = async () => {
   try {
     await mongoose.connect(process.env.DB_URI || "");
+    console.log("接続成功");
   } catch (error) {
     console.log("DB接続に失敗しました");
     throw new Error();
